1b8bfbbf427cfa36243c49321c1247d904a451d8
Dilusha-Madushan <dilushamadushan9912@gmail.com>
2024-07-09 01:08:59 +0530

    Fix bug

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -3,11 +3,12 @@ const { admin } = require('../config/firebaseConfig');
 // Middleware to verify Firebase token (authentication)
 exports.verifyToken = async (req, res, next) => {
   const token = req.headers.authorization?.split('Bearer ')[1];
-  if (!token) {
+  console.log(token)
+  if (token === '') {
     return res.status(403).send({ error: true, message: "No token provided." });
   }
   try {
-    req.user = decodedToken.token;
+    req.user = token;
     next();
   } catch (error) {
     res.status(403).send({ message: "Invalid token.", error: true });
